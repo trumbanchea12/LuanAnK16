33ea2ed977afc20c9c73fe27242e2c0a6495d16f
trieule47 <62042242+trieule47@users.noreply.github.com>
2020-03-15 08:11:39 +0700

    hoang315

--- App.js
+++ App.js
@@ -27,6 +27,41 @@ const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 const Tabs = createBottomTabNavigator();
 
+// Funcion icon Bottom menu
+function IconWithBadge({ name, badgeCount, color, size }) {
+  return (
+    <View style={{ width: 24, height: 24, margin: 5 }}>
+      <Ionicons name={name} size={size} color={color} />
+      {badgeCount > 0 && (
+        <View
+          style={{
+            // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
+            position: 'absolute',
+            right: -6,
+            top: -3,
+            backgroundColor: 'red',
+            borderRadius: 6,
+            width: 12,
+            height: 12,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+            {badgeCount}
+          </Text>
+        </View>
+      )}
+    </View>
+  );
+}
+
+function HomeIconWithBadge(props) {
+  // You should pass down the badgeCount in some other ways like React Context API, Redux, MobX or event emitters.
+  return <IconWithBadge {...props} badgeCount={0} />;
+}
+
+
 class HomeStack extends Component {
   render() {
     return (
